7554ee95410896d5e3657bdccdbd4890808f9f7b
reza <shabaany@yahoo.com>
2020-09-02 23:04:56 +0430

    feat: Add support for Map and Set class
    traverse function has moved to ScriptFromObject class. index.js is
    refactored.

--- src/index.js
+++ src/index.js
@@ -1,50 +1,12 @@
-var objectSet = new Set();
-var { traverse } = require("./helper");
-var classesToScript = require("./toScript")
-
-var replacer = function(key, value) {
-	if (typeof value == "object") {
-		if (objectSet.has(value)) {
-			return;
-		}
-		objectSet.add(value);
-	}
-	if (typeof value == "bigint") return;
-	for (let cls of classesToScript) {
-		if (value instanceof cls.type) return;
-	}
-	return value;
-};
+var ScriptFromObject = require("./ScriptFromObject");
 
 var scriptify = function(obj, options) {
-	options = options || {};
 
-	objectSet.clear();
+	var scriptFromObject = new ScriptFromObject(obj);
 
-	expressionList = traverse(obj, "obj");
+	const script = scriptFromObject.exportAsFunctionCall(options);
 
-	var str = "(function(){\n//version 1\n";
-	str += "  var obj = " + JSON.stringify(obj, replacer) + ";";
-
-	str += "\n//Circular references";
-	expressionList.circularReferences.forEach(([path, reference]) => {
-		str += "\n" + "  " + path + " = " + reference + ";";
-	});
-
-	str += "\n//Object constructors";
-	expressionList.objectConstructors.forEach(([path, code, addExpression]) => {
-		if (addExpression) str += `\n ${path} = ${code}; ${path}${addExpression}`;
-		else str += `\n ${path} = ${code};`;
-	});
-
-	if (options.withAllFunctions) {
-		str += "\n//Functions";
-		expressionList.functionsList.forEach(([path, code]) => {
-			str += "\n" + "  " + path + " = " + code + ";";
-		});
-	}
-	str += "\n  return obj;\n})()";
-	return str;
+	return script;
 };
 
 scriptify.withAllFunctions = function(obj) {
--- src/toScript.js
+++ src/toScript.js
@@ -10,6 +10,26 @@ var classes = [
 		toScript: function(obj) {
 			return obj.toString();
 		}
+	},
+	{
+		type: Map,
+		toScript: function(obj, getScript, varName) {
+			return {
+				empty: `new Map()`,
+				add: `${getScript(
+					Array.from(obj)
+				)}.forEach(([k,v])=>{${varName}.set(k,v);})`
+			};
+		}
+	},
+	{
+		type: Set,
+		toScript: function(obj, getScript, varName) {
+			return {
+				empty: `new Set()`,
+				add: `${getScript(Array.from(obj))}.forEach((v)=>{${varName}.add(v);})`
+			};
+		}
 	}
 ];
 
