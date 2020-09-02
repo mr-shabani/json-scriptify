5f587be920e2ff9599547d08306f6e993a27292b
reza <shabaany@yahoo.com>
2020-09-02 23:07:59 +0430

    test: Add test for Map and Set

--- test/__tests__/built-in_objects.js
+++ test/__tests__/built-in_objects.js
@@ -19,6 +19,54 @@ test("Date class", function() {
 test("RegExp class", function() {
 	expect(checkFor(/abc/)).toEqual(true);
 	expect(checkFor({ r: /abc/gi })).toEqual(true);
-	expect(checkFor(new RegExp('abc','i'))).toEqual(true);
-	expect(checkFor({ r: new RegExp('abc','i')})).toEqual(true);
+	expect(checkFor(new RegExp("abc", "i"))).toEqual(true);
+	expect(checkFor({ r: new RegExp("abc", "i") })).toEqual(true);
+});
+
+test("Map class", function() {
+	expect(
+		checkFor(
+			new Map([
+				[1, 2],
+				[3, 4]
+			])
+		)
+	).toEqual(true);
+	expect(
+		checkFor({
+			m: new Map([
+				[1, 2],
+				[3, 4]
+			])
+		})
+	).toEqual(true);
+});
+
+test("Map class with circular", function() {
+	let map = new Map([
+		[1, 2],
+		[3, 4]
+	]);
+	map.set(map, map);
+	expect(checkFor(map)).toEqual(true);
+	let obj = { map };
+	map.set(obj, obj);
+	expect(checkFor(obj)).toEqual(true);
+});
+
+test("Set class", function() {
+	expect(checkFor(new Set([1, 2, 3, 4]))).toEqual(true);
+	expect(checkFor({ s: new Set([1, 2, 3, 4]) })).toEqual(true);
+});
+
+test("Set class with circular", function() {
+	let set = new Set([
+		[1, 2],
+		[3, 4]
+	]);
+	set.add(set);
+	expect(checkFor(set)).toEqual(true);
+	let obj = { set };
+	set.add(obj);
+	expect(checkFor(obj)).toEqual(true);
 });
