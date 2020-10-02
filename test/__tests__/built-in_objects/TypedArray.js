0fe81187dd44ebff5c57aed439ee11435732d8d7
reza <shabaany@yahoo.com>
2020-10-02 16:51:02 +0330

    fix: Correct TypedArray tests

--- test/__tests__/built-in_objects/TypedArray.js
+++ test/__tests__/built-in_objects/TypedArray.js
@@ -10,7 +10,7 @@ var checkFor = function(obj) {
 };
 
 test("Uint8Array object", function() {
-	var obj = new Uint8Array([1,2,3,4,5]).buffer;
+	var obj = new Uint8Array([1,2,3,4,5]);
 	expect(checkFor(obj)).toEqual(true);
 	expect(checkFor({ a: obj })).toEqual(true);
 	obj[10] = 89;
@@ -22,29 +22,31 @@ test("Uint8Array object", function() {
 });
 
 test("Uint8Array object with byteOffset", function() {
-	var obj = new Uint8Array([0,1,2,3,4,5,7,8,9]).buffer;
+	var buff = Uint8Array.from([0,1,2,3,4,5,7,8,9]).buffer;
+	expect(checkFor(buff)).toEqual(true);
+	expect(checkFor({ a: buff })).toEqual(true);
+    var obj = new Uint8Array(buff,3,4);
+    expect(obj.buffer).toEqual(buff);
+
 	expect(checkFor(obj)).toEqual(true);
 	expect(checkFor({ a: obj })).toEqual(true);
-    var obj2 = new Uint8Array(obj,3,4);
-    expect(obj2.buffer).toEqual(obj);
-
-	expect(checkFor(obj2)).toEqual(true);
-	expect(checkFor({ a: obj2 })).toEqual(true);
+	obj[4]=10;
 	obj.x = 2;
-	expect(checkFor(obj2)).toEqual(true);
-	expect(checkFor({ a: obj2 })).toEqual(true);
+	expect(checkFor(obj)).toEqual(true);
+	expect(checkFor({ a: obj })).toEqual(true);
 });
 
 test("Uint32Array object with byteOffset", function() {
-	var obj = new Uint32Array([0,1,2,3,4,5,7,8,9]).buffer;
+	var buff = Uint32Array.from([0,1,2,3,4,5,7,8,9]).buffer;
+	expect(checkFor(buff)).toEqual(true);
+	expect(checkFor({ a: buff })).toEqual(true);
+    var obj = new Uint32Array(buff,4,4);
+    expect(obj.buffer).toEqual(buff);
+
 	expect(checkFor(obj)).toEqual(true);
 	expect(checkFor({ a: obj })).toEqual(true);
-    var obj2 = new Uint32Array(obj,4,4);
-    expect(obj2.buffer).toEqual(obj);
-
-	expect(checkFor(obj2)).toEqual(true);
-	expect(checkFor({ a: obj2 })).toEqual(true);
+	obj[4]=10;
 	obj.x = 2;
-	expect(checkFor(obj2)).toEqual(true);
-	expect(checkFor({ a: obj2 })).toEqual(true);
+	expect(checkFor(obj)).toEqual(true);
+	expect(checkFor({ a: obj })).toEqual(true);
 });
